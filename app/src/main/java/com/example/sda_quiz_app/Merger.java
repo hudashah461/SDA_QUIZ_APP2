cf9cf02099f831c2cfc09fbbf000537d957f01de
NOOR UL HUDA SHAH <hudashah461@gmail.com>
2019-05-30 02:37:17 +0500

    normal_result class

--- app/src/main/java/com/example/sda_quiz_app/Merger.java
+++ app/src/main/java/com/example/sda_quiz_app/Merger.java
@@ -1,4 +1,11 @@
 package com.example.sda_quiz_app;
 
+import android.content.Context;
+
 public class Merger {
+    private static Context context;
+
+    public Merger(Context context){
+        this.context = context;
+    }
 }
