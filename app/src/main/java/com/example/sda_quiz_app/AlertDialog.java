01013fbba2c7a0439cc286d309e2b060f48f1a1f
NOOR UL HUDA SHAH <hudashah461@gmail.com>
2019-05-30 02:25:24 +0500

    data helper class

--- app/src/main/java/com/example/sda_quiz_app/AlertDialog.java
+++ app/src/main/java/com/example/sda_quiz_app/AlertDialog.java
@@ -1,5 +1,13 @@
 package com.example.sda_quiz_app;
 
-public class AlertDialog
-{
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AlertDialog extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alert_dialog);
+    }
 }
